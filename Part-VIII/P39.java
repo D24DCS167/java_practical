e4a240fc426aab8ad278a2d1da5457fa301ffb75
D24DCS167 <d24dcs167@charusat.edu.in>
2024-10-19 23:26:40 +0530

    Add files via upload

---
+++ Part-VIII/P39.java
@@ -0,0 +1,11 @@
+/**Imagine you are developing an e-commerce application. 
+The platform needs to sort lists of products based on 
+different criteria, such as price, rating, or name. Each 
+product object implements the Comparable interface to 
+define the natural ordering. To ensure flexibility and 
+reusability, you need a generic method that can sort any 
+array of Comparable objects. Create a generic method in 
+Java that sorts an array of Comparable objects. This method 
+should be versatile enough to sort arrays of different types 
+of objects (such as products, customers, or orders) as long 
+as they implement the Comparable interface.  */
